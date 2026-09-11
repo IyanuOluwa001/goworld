0cf8fb16e3daae83862339c2ba59710547126007
iadejare <iyanuoluwaadejare2025@gmail.com>
2026-09-11 14:48:03 +0100

    Add gopractice

---
+++ gopractice/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	familyName("Tse")
+	familyName("Saidat")
+	familyName("Gee")
+}
---
+++ gopractice/myargs.go
@@ -0,0 +1,6 @@
+package main
+import ("fmt")
+
+func familyName(fname string){
+	fmt.Println("Hello", fname, "there")
+}
